1c50d56072a2a3b1634a6ed88a350c47c038fd1b
Konstantin Dyachenko <drakonard@gmail.com>
2023-12-16 02:31:52 +0300

    Add ListAllKeyRates method

--- Program.cs
+++ Program.cs
@@ -46,4 +46,37 @@
     }
     Console.WriteLine($"Last key rate {lastRate} (effective from {lastDate.ToString("s")})");
   }
+  private static void ListAllKeyRates(object? stateInfo)
+  {
+    var client = new DailyInfoSoapClient(DailyInfoSoapClient.EndpointConfiguration.DailyInfoSoap12);
+    var response = client.KeyRateAsync(new DateTime(2023, 08, 01), new DateTime(2023, 12, 19)).Result;
+
+    foreach (var element in response.Nodes)
+    {
+      if (element.Name.LocalName == "diffgram")
+      {
+        foreach (var keyRate in element.Elements())
+        {
+          foreach (var kr in keyRate.Elements())
+          {
+            var date = DateTime.Now;
+            var rate = 0.0M;
+
+            foreach (var innerElement in kr.Elements())
+            {
+              if (innerElement.Name.LocalName == "DT")
+              {
+                DateTime.TryParse(innerElement.Value, out date);
+              }
+              if (innerElement.Name.LocalName == "Rate")
+              {
+                decimal.TryParse(innerElement.Value, out rate);
+              }
+            }
+            Console.WriteLine($"{date.ToString("s")}: {rate}");
+          }
+        }
+      }
+    }
+  }
 }
